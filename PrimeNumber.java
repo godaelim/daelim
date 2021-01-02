ac7835f3f29269a54a0e3187d4ee9fcd6a879b4c
godaelim <2009564@daelim.ac.kr>
2021-01-02 10:04:04 +0900

    v6 flag variable isPrime

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -5,17 +5,20 @@ public class PrimeNumber {
 		Scanner scv = new Scanner(System.in);
 		int n = scv.nextInt();
 		
-		int count = 0;
+		//int count = 0;
+		boolean isPrime = true;
 		
 		for(int k=2; k<n; k++) {
 			if(n%k == 0){ 
-				count++;
+				//count++;
+				isPrime = false;
 				break;
 			}
-			System.out.println(k);
+			//System.out.println(k);
 		}
 		
-		if(count == 0)
+		//if(count == 0)
+		if(isPrime)
 			System.out.println(n + " is prime number!");
 		else
 			System.out.println(n + " is NOT prime number.");
