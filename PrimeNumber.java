8dd2d40355c081f08fcb242fe1cd8cc1f0e93ecd
godaelim <2009564@daelim.ac.kr>
2021-01-02 09:32:20 +0900

    v2 count 주석처리, 인코딩처리

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -5,17 +5,16 @@ public class PrimeNumber {
 		Scanner scv = new Scanner(System.in);
 		int n = scv.nextInt();
 		
-		// 1과 자기 자신외에는 나누어 떨어지지 않는 수. 소수.
 		int count = 0;
 		
 		for(int k=1; k<=n; k++) {
-			if(n%k == 0) // 1부터 1씩 증가하며 입력 받은 수가 나누어 떨어지는 횟수를 카운트
+			if(n%k == 0) 
 				count++;
 		}
 		
 		if(count == 2)
-			System.out.println(n + "은 소수!");
+			System.out.println(n + " is prime number!");
 		else
-			System.out.println(n + "은 소수가 아닙니다.");
+			System.out.println(n + " is NOT prime number.");
 	}
 }
